90d5571b91aa63502143842e445e141350218ba6
shantovai23 <hasibulhasan23sept@gmail.com>
2021-06-20 22:44:48 +0600

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import  express from 'express'
 import  dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 import  products from './data/products.js'
 
 
 dotenv.config()
+
+connectDB()
+
 const app=express()
 
 app.get('/',(req,res)=>{
@@ -22,4 +27,4 @@ app.get('/api/products/:id',(req,res)=>{
  const PORT=process.env.PORT || 5000
 
 
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.brightBlue.bold))
