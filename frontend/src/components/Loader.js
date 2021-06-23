d460a0ba3bb77a0e1056b5240bc4fe01d7b13094
shantovai23 <hasibulhasan23sept@gmail.com>
2021-06-23 21:11:51 +0600

    redux implemented

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Spinner } from "react-bootstrap";
+
+const Loader = () => {
+  return (
+    <Spinner
+      animation="border"
+      variant="primary"
+      role="status"
+      style={{ width: "40px", height: "40px", margin: "auto",display:'block' }}
+    >
+    <span class='sr-only'>Loading...</span>
+    </Spinner>
+    
+  );
+};
+
+export default Loader;
