8d9b9e79a7d462ee37e585fe83e99f44b1a3f32c
shantovai23 <hasibulhasan23sept@gmail.com>
2021-07-08 13:19:26 +0600

    admin functionality continue

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -120,4 +120,21 @@ const getUsers = asyncHandler(async (req, res) => {
   // console.log(users.length)
 });
 
-export { authUser, registerUser, getUserProfile,updateUserProfile,getUsers };
+//DELETE  user
+//DELETE/api/users/:id
+//access private/admin
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if(user){
+   await user.remove()
+   res.json({message:'User Removed'})
+  }else{
+    res.status(404)
+    throw new Error('user not found')
+  }
+  // console.log(users.length)
+});
+
+export { authUser, registerUser, getUserProfile,updateUserProfile,getUsers,deleteUser };
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,24 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import {LinkContainer} from 'react-router-bootstrap'
 import { Table, Button,} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import {listUsers} from '../actions/userActions'
-import { deleteModel } from "mongoose";
+import {listUsers,deleteUser} from '../actions/userActions'
 
-const UserListScreen = () => {
+
+const UserListScreen = ({history}) => {
     const dispatch=useDispatch()
     const userList=useSelector((state)=>state.userList)
     const {users,error,loading}=userList
 
-    const deleteHandler=(e)=>{
+    const userLogin=useSelector((state)=>state.userLogin)
+    const {userInfo}=userLogin
 
+    const userDelete=useSelector((state)=>state.userDelete)
+    const {success:successDelete}=userDelete
+
+    const deleteHandler=(id)=>{
+        if(window.confirm('Are You Sure?')){
+            dispatch(deleteUser(id))
+        }
+     
     }
 
     useEffect(()=>{
-        dispatch(listUsers())
-    },[dispatch])
+        if(userInfo && userInfo.isAdmin){
+            dispatch(listUsers())
+        }else{
+        history.push('/login')
+        }
+       
+    },[dispatch,history,userInfo,successDelete])
     return (
         <>  
             <h4>Users</h4>
